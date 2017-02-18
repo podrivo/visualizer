1246df2220fda57996c925f9df2a6ea4557b9a7d
Pedro Ivo Hudson <pivo@hugeinc.com>
2017-02-18 20:35:55 -0200

    click to create random dots

--- public/scripts/first.js
+++ public/scripts/first.js
@@ -1,18 +1,38 @@
-(function(){
 
-  // console.log('yo');
+var container = document.getElementById('container');
+var dotsArr = [];
 
-  var container = document.getElementById('container');
-
-
-
-
-})()
 function createDots() {
-  dot = document.createElement('i');
+  // var randomKey = Math.random().toString(36).substring(7);
+  // console.log(randomKey);
+
+  var dot = document.createElement('i');
+  // dot.setAttribute('id',randomKey);
   container.appendChild(dot);
-  console.log('teste');
+  dotsArr.push(dot);
+  // console.log(dotsArr);
+
+  var x = getRandomInt(-220,220);
+  var y = getRandomInt(-220,220);
+
+  TweenMax.to(dot, 2, {
+    transform: 'translate(' + x + 'px,' + y + 'px)',
+    onComplete: fim,
+    // delay: 0.5
+  });
+  console.log('dot created', x, y);
 }
+
+function fim() {
+  console.log('animacao acabou');
+  console.log(getRandomInt(60,420));
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 
 function preload() {
   sound = loadSound('audio/song01.mp3');
@@ -24,6 +44,12 @@ function setup() {
 }
 
 function draw() {
-  console.log('yo');
-  createDots();
+  // console.log('yo');
+  if (mouseIsPressed) {
+    createDots();
+    // fill(0);
+  } else {
+    // fill(255);
+  }
+
 }
