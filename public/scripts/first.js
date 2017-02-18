ddfc381d28fee4a0116f15a43f93e8df8f419f5f
Pedro Ivo Hudson <pivo@hugeinc.com>
2017-02-18 16:51:32 -0200

    config p5,p5.sonud,TweenMax

--- public/scripts/first.js
+++ public/scripts/first.js
@@ -1,5 +1,29 @@
 (function(){
 
-  console.log('yo');
-  
+  // console.log('yo');
+
+  var container = document.getElementById('container');
+
+
+
+
 })()
+function createDots() {
+  dot = document.createElement('i');
+  container.appendChild(dot);
+  console.log('teste');
+}
+
+function preload() {
+  sound = loadSound('audio/song01.mp3');
+}
+
+function setup() {
+  createCanvas(0,0);
+  // sound.play();
+}
+
+function draw() {
+  console.log('yo');
+  createDots();
+}
