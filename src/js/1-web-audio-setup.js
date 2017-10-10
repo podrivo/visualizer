691344363c9b1949c12415d6fea3f7a907597fb2
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2017-10-10 20:48:00 -0300

    cleaning :clap:

--- src/js/1-web-audio-setup.js
+++ src/js/1-web-audio-setup.js
@@ -2,7 +2,7 @@
 var audio, fps, context, source, analyser, frequencyData, gainNode;
 
 // var trackUrl = streamUrl;
-var trackUrl = 'https://api.soundcloud.com/tracks/284720208/stream?client_id=eab076c133468510a6efbe8ca1390e96';
+// var trackUrl = 'https://api.soundcloud.com/tracks/284720208/stream?client_id=eab076c133468510a6efbe8ca1390e96';
 // var trackUrl = 'https://api.soundcloud.com/tracks/311734519/stream?client_id=eab076c133468510a6efbe8ca1390e96';
 var trackUrl = 'https://api.soundcloud.com/tracks/268435373/stream?client_id=eab076c133468510a6efbe8ca1390e96';
 
@@ -27,14 +27,14 @@ function webAudioSetup() {
   });
 };
 
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open('GET', 'https://api.soundcloud.com/playlists/405726?client_id=eab076c133468510a6efbe8ca1390e96', true);
-  xhttp.send();
-};
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("demo").innerHTML =
+//       this.responseText;
+//     }
+//   };
+//   xhttp.open('GET', 'https://api.soundcloud.com/playlists/405726?client_id=eab076c133468510a6efbe8ca1390e96', true);
+//   xhttp.send();
+// };
