b7b7a64327176edae79a566bdaea5ce386685809
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2017-10-10 02:05:08 -0300

    new range of possibilities :orange_book:

--- src/js/1-web-audio-setup.js
+++ src/js/1-web-audio-setup.js
@@ -1,5 +1,5 @@
 
-var audio, fps, context, source, analyser, frequencyData;
+var audio, fps, context, source, analyser, frequencyData, gainNode;
 
 // var trackUrl = streamUrl;
 var trackUrl = 'https://api.soundcloud.com/tracks/284720208/stream?client_id=eab076c133468510a6efbe8ca1390e96';
@@ -13,8 +13,13 @@ function webAudioSetup() {
   audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
   source = audioContext.createMediaElementSource(audio);
-  analyser = audioContext.createAnalyser();
   source.connect(audioContext.destination);
+
+  gainNode = audioContext.createGain();
+  gainNode.connect(audioContext.destination);
+  source.connect(gainNode);
+
+  analyser = audioContext.createAnalyser();
   source.connect(analyser);
 
   document.body.addEventListener('click', function(){
--- src/js/2-sound-play-pause.js
+++ src/js/2-sound-play-pause.js
@@ -4,6 +4,7 @@ function soundPlayPause() {
   // if paused, start to play
   if (audio.paused) {
     audio.play();
+    gainNode.gain.value = -1;
 
   // if playing, pause and shrink dots
   } else {
