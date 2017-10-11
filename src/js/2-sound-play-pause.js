78bf73c879f96af1ea699a6a6fbbbeefdb4b0558
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2017-10-10 21:33:03 -0300

    hiding 'mute' :mute:

--- src/js/2-sound-play-pause.js
+++ src/js/2-sound-play-pause.js
@@ -4,7 +4,7 @@ function soundPlayPause() {
   // if paused, start to play
   if (audio.paused) {
     audio.play();
-    gainNode.gain.value = -1;
+    // gainNode.gain.value = -1; // mute
 
   // if playing, pause and shrink dots
   } else {
